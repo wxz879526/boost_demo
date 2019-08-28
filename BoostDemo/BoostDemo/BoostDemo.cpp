7126c1b748d4edc617611b79273ae335aada4bfb
My Name <my-name@chromium.org>
2019-08-28 23:09:00 +0800

    update

--- BoostDemo/BoostDemo/BoostDemo.cpp
+++ BoostDemo/BoostDemo/BoostDemo.cpp
@@ -4,9 +4,20 @@
 #include "pch.h"
 #include <iostream>
 
+#include "boost/log/trivial.hpp"
+
 int main()
 {
     std::cout << "Hello World!\n"; 
+
+	BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
+	BOOST_LOG_TRIVIAL(debug) << "A debug severity message";
+	BOOST_LOG_TRIVIAL(info) << "An informational severity message";
+	BOOST_LOG_TRIVIAL(warning) << "A warning severity message";
+	BOOST_LOG_TRIVIAL(error) << "An error severity message";
+	BOOST_LOG_TRIVIAL(fatal) << "A fatal severity message";
+
+	return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
