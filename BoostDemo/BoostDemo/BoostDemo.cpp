6572af7ab81bd9f3f783ccc575b2b83b1e4827b4
My Name <my-name@chromium.org>
2019-08-29 23:15:13 +0800

    add sinks

--- BoostDemo/BoostDemo/BoostDemo.cpp
+++ BoostDemo/BoostDemo/BoostDemo.cpp
@@ -4,13 +4,23 @@
 #include "pch.h"
 #include <iostream>
 
-#include "boost/log/trivial.hpp"
-#include "boost/log/core.hpp"
-#include "boost/log/expressions.hpp"
+#include <boost/log/core.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sinks/text_file_backend.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/sources/severity_logger.hpp>
+#include <boost/log/sources/record_ostream.hpp>
 
 // 设置过滤级别
 void init()
 {
+	boost::log::add_file_log(boost::log::keywords::file_name="sample_%N.log",
+		boost::log::keywords::rotation_size = 10 * 1024 * 1024,
+		boost::log::keywords::time_based_rotation = boost::log::sinks::file::rotation_at_time_point(0,0,0),
+		boost::log::keywords::format = "[%TimeStamp%]: %Message%");
+
 	boost::log::core::get()->set_filter(
 		boost::log::trivial::severity >= boost::log::trivial::error
 	);
@@ -19,6 +29,9 @@ void init()
 int main()
 {
 	init();
+
+	// 调用这接口 [%TimeStamp%]: %Message% 才有效
+	boost::log::add_common_attributes();
 
 	BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
 	BOOST_LOG_TRIVIAL(debug) << "A debug severity message";
