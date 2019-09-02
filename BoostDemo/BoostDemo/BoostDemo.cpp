213d1f82608c1f77acaa2231e04a2521c0840c92
My Name <my-name@chromium.org>
2019-09-02 23:00:52 +0800

    variant2 test

--- BoostDemo/BoostDemo/BoostDemo.cpp
+++ BoostDemo/BoostDemo/BoostDemo.cpp
@@ -2,54 +2,85 @@
 //
 
 #include "pch.h"
+
+#define _USE_MATH_DEFINES
 #include <iostream>
+#include <vector>
+#include <memory>
+#include <string>
+#include <cmath>
 
-#include <boost/log/core.hpp>
-#include <boost/log/trivial.hpp>
-#include <boost/log/expressions.hpp>
-#include <boost/log/sinks/text_file_backend.hpp>
-#include <boost/log/utility/setup/file.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
-#include <boost/log/sources/severity_logger.hpp>
-#include <boost/log/sources/record_ostream.hpp>
+#include "boost/variant2/variant.hpp"
 
-// 设置过滤级别
-void init()
+
+class Rect
 {
-	boost::log::add_file_log(boost::log::keywords::file_name="sample_%N.log",
-		boost::log::keywords::rotation_size = 10 * 1024 * 1024,
-		boost::log::keywords::time_based_rotation = boost::log::sinks::file::rotation_at_time_point(0,0,0),
-		boost::log::keywords::format = "[%TimeStamp%]: %Message%");
+private:
+	double width_, height_;
 
-	boost::log::core::get()->set_filter(
-		boost::log::trivial::severity >= boost::log::trivial::error
-	);
+public:
+	Rect(double width, double height)
+		: width_(width)
+		, height_(height)
+	{
+
+	}
+
+	double Area() const
+	{
+		return width_ * height_;
+	}
+};
+
+class Circle
+{
+private:
+	double radius_;
+
+public:
+	Circle(double r)
+		: radius_(r)
+	{
+
+	}
+
+	double Area() const
+	{
+		return M_PI * radius_ * radius_;
+	}
+};
+
+double total_area(const std::vector<boost::variant2::variant<Rect, Circle>> &v)
+{
+	double s = 0.0;
+	for (const auto &p : v)
+	{
+		auto p1 = boost::variant2::get_if<0>(&p);
+		s += boost::variant2::visit([](auto const &y) {return y.Area(); }, p);
+	}
+
+	return s;
 }
 
 int main()
 {
-	init();
+	boost::variant2::variant<double, char, std::string> v1;
+	v1 = "123";
+	
+	try
+	{
+		auto c = boost::variant2::get<2>(v1);
+	}
+	catch (const boost::variant2::bad_variant_access& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
 
-	// 调用这接口 [%TimeStamp%]: %Message% 才有效
-	boost::log::add_common_attributes();
+	std::vector<boost::variant2::variant<Rect, Circle>> v;
+	v.push_back(Circle(1.0));
+	v.push_back(Rect(2.0, 3.0));
 
-	BOOST_LOG_TRIVIAL(trace) << "A trace severity message";
-	BOOST_LOG_TRIVIAL(debug) << "A debug severity message";
-	BOOST_LOG_TRIVIAL(info) << "An informational severity message";
-	BOOST_LOG_TRIVIAL(warning) << "A warning severity message";
-	BOOST_LOG_TRIVIAL(error) << "An error severity message";
-	BOOST_LOG_TRIVIAL(fatal) << "A fatal severity message";
+	std::cout << "Total area: " << total_area(v) << std::endl;
 
 	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
